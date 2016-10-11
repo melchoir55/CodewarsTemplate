d8667ffaa6a2ae5086d5dd417d178b215ac5434d
Isaac Martin <melchoir55@gmail.com>
2016-10-11 00:30:42 -0700

    Initial project skeleton

---
+++ src/test/SolutionTest.java
@@ -0,0 +1,13 @@
+package test;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class SolutionTest {
+    @Test
+    public void testSomething() {
+       assertEquals("expected", "actual");
+    }
+}
